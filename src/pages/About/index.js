e4061376a9786e0c1c3da2df7b38615a8f11a80a
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-05-21 00:09:10 -0300

    ulitmo commit

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -8,7 +8,7 @@ export default function About() {
             <AboutHeader>
                 <h1>Sobre o Beatmaking</h1>
             </AboutHeader>
-            {/* <AboutMain>
+            <AboutMain>
                 <h1>O que é Beatmaking?</h1>
                 <h2>Beatmaking é o processo de construção de instrumentais com elementos percussivos a partir de uma melodia. Ele sequencia loops compostos com instrumentação ao vivo e/ou virtual, sampleando outros artistas ou misturando essas duas formas.</h2>
                 <h1>História do Beatmaking</h1>
@@ -16,12 +16,12 @@ export default function About() {
                 <h2>Estas técnicas não eram músicas em sentido estrito, mas ritmos “improvisados” em festas nichadas dos anos 70, festas com 12 horas de duração muitas vezes. Percebendo o sucesso desses ritmos, surgiram gravadoras interessadas em transformar esses ritmos em músicas comerciais, mas havia um problema... Como transformar todos aqueles sons que eram tocados durante horas de forma totalmente não coesa em uma música de poucos minutos com um formato comercial? Houveram algumas tentativas nas quais eles começaram a gravar os sons daquelas festas e depois os cortavam e compactavam de forma a transformá-los em músicas comerciais, mas estavam enfrentando muitas dificuldades no processo, tanto no sentido artístico de adequação quanto no jurídico, pois começaram a enfrentar problemas relacionados a direitos autorais, já que os Breaks eram partes de músicas já existentes.</h2>
                 <h2>Mas em meados da década de 1980, surgiu uma nova tecnologia que permitiria que o Hip Hop amadurecesse como gênero musical: samplers digitais. Essa tecnologia também solucionou o problema das gravadoras, pois permitia que os produtores criassem sons de instrumentos músicais de forma digital, tendo uma capacidade de manipulação e transformação destes muito maior.</h2>
                 <h2>Com a criação destes samplers surgiram os famosos beatmakers, os produtores das beats. E assim se estabeleceu o processo de evolução do Beatmaking até como o conhecemos hoje, com instrumentos muito mais complexos e tecnológicos.</h2>
-            </AboutMain> */}
-            {/* <Button>
+            </AboutMain>
+            <Button>
                 <Link to="/choice" style={{ textDecoration: 'none' }}>
                     <ion-icon name="arrow-forward-outline"></ion-icon>
                 </Link>
-            </Button> */}
+            </Button>
         </AboutBody>
     );
 }
