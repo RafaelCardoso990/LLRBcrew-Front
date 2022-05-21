e46b4a6b4b0843ab245eb01be04c80988cae52d2
Leonardo Arantes <leonardorarantes95@gmail.com>
2022-05-20 23:52:36 -0300

    feat: adds Login Page and About Page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {ParamContext} from "./assets/context/ParamContext";
-import {useState} from "react";
+import { ParamContext } from "./assets/context/ParamContext";
+import { useState } from "react";
 
 import Choice from "./pages/choice";
-<<<<<<< HEAD
+import Tools from "./pages/tools";
 import Login from "./pages/Login";
 import About from "./pages/About";
 
+
 function App() {
+    const [param, setParam] = useState();
     return (
-
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Login />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/choice' element={<Choice />} />
-            </Routes>
-        </BrowserRouter>
-
-=======
-import Tools from "./pages/tools";
-
-
-function App(){
-    const [param,setParam] = useState();
-    return(
-       <ParamContext.Provider value={{param,setParam}}>
+        <ParamContext.Provider value={{ param, setParam }}>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/choice' element={<Choice />} />      
-                    <Route path='/tool' element={<Tools />} />                 
+                    <Route path='/' element={<Login />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/choice' element={<Choice />} />
+                    <Route path='/tool' element={<Tools />} />
                 </Routes>
             </BrowserRouter>
         </ParamContext.Provider>
->>>>>>> cc689182f94cf51de2cc326b8b60593eab8eafba
     )
 }
 
